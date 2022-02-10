d001c8fa2143ed6143d02ccbdd935e1aecd685cd
playfix9 <98113877+playfix9@users.noreply.github.com>
2022-02-10 17:38:30 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Clase-9
-Clase-9 Practica
+# PROC9_P5Juego
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var cubo;
+
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  cubo=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)) {
+  cubo.position.x+=5;
+}
+
+if(keyIsDown(LEFT_ARROW)) {
+  cubo.position.x-=5;
+}
+
+if(keyIsDown(UP_ARROW)) {
+  cubo.position.y-=5;
+}
+
+if(keyIsDown(DOWN_ARROW)) {
+  cubo.position.y+=5;
+}
+
+drawSprites();
+}
+
+
+
+
